143195341ca96fcfe77d9bacfdc34b32bcbd5e1c
Lawrence Buta <larry.buta@gmail.com>
2022-07-04 17:46:42 -0400

    Mathificent-07-04-2022-Push-Two

---
+++ src/components/ClearButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ClearButton(props) {
+  return (
+    <button className="btn btn-primary" style={{ width: "4.2em" }}>
+      Clear
+    </button>
+  );
+}
+
+export default ClearButton;
---
+++ src/components/Timer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Timer(props) {
+  return <strong>Time: {props.timeLeft}</strong>;
+}
+
+export default Timer;
