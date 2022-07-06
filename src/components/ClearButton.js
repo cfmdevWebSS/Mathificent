410c532f5466d680ade3472afcb1a2226c5194bb
Lawrence Buta <larry.buta@gmail.com>
2022-07-06 18:42:22 -0400

    Mathificent-07-06-2022

--- src/components/ClearButton.js
+++ src/components/ClearButton.js
@@ -2,7 +2,13 @@ import React from "react";
 
 function ClearButton(props) {
   return (
-    <button className="btn btn-primary" style={{ width: "4.2em" }}>
+    <button
+      className="btn btn-primary"
+      style={{ width: "4.2em" }}
+      onClick={() => {
+        props.handleClick("");
+      }}
+    >
       Clear
     </button>
   );
--- src/components/Game.js
+++ src/components/Game.js
@@ -7,10 +7,56 @@ import ClearButton from "./ClearButton";
 import "./Game.css";
 import { randInt } from "../helpers/helpers";
 
-function Game(operation, maxNumber) {
+function Game({ operation, maxNumber }) {
   let randNums = getRandNumbers(operation, 0, maxNumber);
   const [operands, setOperands] = useState(randNums);
   const question = operands.num1 + " " + operation + " " + operands.num2;
+  const [userAnswer, setUserAnswer] = useState("");
+  const [answered, setAnswered] = useState(false);
+  const [score, setScore] = useState(0);
+
+  function appendToAnswer(num) {
+    setUserAnswer(String(Number(userAnswer + num)));
+  }
+
+  function checkAnswer(userAnswer) {
+    if (isNaN(userAnswer)) return false;
+
+    let correctAnswer;
+
+    switch (operation) {
+      case "+":
+        correctAnswer = operands.num1 + operands.num2;
+        break;
+      case "-":
+        correctAnswer = operands.num1 - operands.num2;
+        break;
+      case "x":
+        correctAnswer = operands.num1 * operands.num2;
+        break;
+      default:
+        correctAnswer = operands.num1 / operands.num2;
+        break;
+    }
+    return parseInt(userAnswer) === correctAnswer;
+  }
+
+  if (!answered && checkAnswer(userAnswer)) {
+    setAnswered(true);
+    setScore(score + 1);
+    setTimeout(newQuestion, 300);
+  }
+
+  function newQuestion() {
+    setUserAnswer("");
+    setAnswered(false);
+    randNums = getRandNumbers(operation, 0, maxNumber);
+    setOperands(randNums);
+  }
+
+  const equationClass = answered
+    ? "row my-2 text-primary fade"
+    : "row my-2 text-secondary";
 
   function getRandNumbers(operator, low, high) {
     let num1 = randInt(low, high);
@@ -33,28 +79,29 @@ function Game(operation, maxNumber) {
     }
     return { num1, num2 };
   }
-  // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  // const numberButtons = numbers.map((number) => (
-  //   <NumberButton value={number} key={number} />
-  // ));
+
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const numberButtons = numbers.map((number) => (
+    <NumberButton value={number} key={number} handleClick={appendToAnswer} />
+  ));
 
   return (
     <main className="text-center" id="game-container">
       <div className="row border-bottom" style={{ fontSize: "1.5em" }}>
         <div className="col px-3 text-left">
-          <Score score="0" />
+          <Score score={score} />
         </div>
         <div className="col px-3 text-right">
           <Timer timeLeft="60" />
         </div>
       </div>
-      <div className="row text-secondary my-2" id="equation">
-        <Equation question={question} answer="2" />
+      <div className={equationClass} id="equation">
+        <Equation question={question} answer={userAnswer} />
       </div>
       <div className="row" id="buttons">
         <div className="col">
-          {/* {numberButtons} */}
-          <ClearButton />
+          {numberButtons}
+          <ClearButton handleClick={setUserAnswer} />
         </div>
       </div>
     </main>
